f8a7d7a15c741a28a5cf0af45b3716c783498cbd
jayko <jayko95@naver.com>
2021-07-08 13:04:21 +0900

    make login

---
+++ users/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms.widgets import PasswordInput
+from . import models
+
+class LoginForm(forms.Form):
+
+    email = forms.CharField()
+    password = forms.CharField(widget=PasswordInput)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong"))
+
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("Email does not exist"))
+
+
